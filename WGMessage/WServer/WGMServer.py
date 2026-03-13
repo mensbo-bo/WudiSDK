5d9305e1de08927901f7b13f933832545d5985fb
mebo <fajar100800@gmail.com>
2026-03-13 21:15:33 +0900

    Initialize Asyncronouse TCP Server

--- WGMessage/WServer/WGMServer.py
+++ WGMessage/WServer/WGMServer.py
@@ -23,4 +23,34 @@ class WSServer:
             self.setClient[hexUUID[:5]] = hexUUID
 
         return self.setClient
+
+
+class AsyncServer(asyncio.Protocol):
+    def connection_made(self, transport: asyncio.BaseTransport) -> None:
+        peername = transport.get_extra_info("peername")
+        self.transport = transport
+    
+    def connection_lost(self, exc: Exception | None) -> None:
+        print("Connection lost")
+        print(f"Error : {exc}")
+    
+    def data_received(self, data: bytes) -> None:
+        print(data)
+    
+class TCPServer:
+    def __init__(self, verbose: bool) -> None:
+        self.verbose = verbose
+    
+    async def TCPRun(self, host:str, port:int) -> None:
+        # Get event loop
+        loop = asyncio.get_running_loop()
+        
+        server = await loop.create_server(
+            AsyncServer, host=host, port=port
+        )
+
+        async with server:
+            await server.serve_forever()
+            
+
     
