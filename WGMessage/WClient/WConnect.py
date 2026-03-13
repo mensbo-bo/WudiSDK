373032bb6be7773ba823886395bbdaeb20b90857
mebo <fajar100800@gmail.com>
2026-03-13 15:51:35 +0900

    Initial commit

---
+++ WGMessage/WClient/WConnect.py
@@ -0,0 +1,16 @@
+#python 3.12.3
+
+""" main libraries project """
+import asyncio
+import aiohttp
+
+""" DEPENDECIES """
+import ssl
+
+class Conn:
+    def __init__(self):
+        "This class provide connection to the server WudiGraph"
+        pass
+    def SetToken(self,token_key, token_public):
+        pass
+
---
+++ WGMessage/WServer/WGMessageServer.py
@@ -0,0 +1,10 @@
+import asyncio
+import aiohttp
+
+
+class MessageServer:
+    def __init__(self) -> None:
+        pass
+
+    async def RunServer(self, host : str, port : int) -> None:
+        pass
