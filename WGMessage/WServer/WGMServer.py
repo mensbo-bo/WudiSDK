18e105d748b211120943372c162f02cdb35f3b27
mebo <fajar100800@gmail.com>
2026-03-13 16:33:41 +0900

    Add Websocket Server functionality

--- WGMessage/WServer/WGMServer.py
+++ WGMessage/WServer/WGMServer.py
@@ -1,3 +1,26 @@
 import asyncio
 import aiohttp
 
+import uuid
+
+class WSServer:
+    def __init__(self) -> None:
+        self.ServerToken = uuid.uuid4().hex
+        self.setClient = {}
+
+    async def WSRun(self, host : str, port : int ) -> None:
+        pass
+
+    async def WSGetToken(self) -> str:
+        return self.ServerToken
+    
+    async def GetTokenClient(self) -> dict:
+        return self.setClient
+    
+    async def GenTokenClient(self, numClient : int) -> dict:
+        for i in range(numClient):
+            hexUUID = uuid.uuid4().hex
+            self.setClient[hexUUID[:5]] = hexUUID
+
+        return self.setClient
+    
