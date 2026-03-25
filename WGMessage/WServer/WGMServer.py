c41b4c0bdf45d49a8946beeb1a55c7d2bd9c269b
mebo <fajar100800@gmail.com>
2026-03-25 22:48:59 +0900

    Add utils for websocket

--- WGMessage/WServer/WGMServer.py
+++ WGMessage/WServer/WGMServer.py
@@ -3,28 +3,44 @@ import aiohttp
 
 import uuid
 
+from typing import Any
+from __pstrogeWServer import __ws
+import WGMessage.WServer.utils as utils
+
 class WSServer:
     def __init__(self) -> None:
-        self.ServerToken = uuid.uuid4().hex
+        self.ServerToken = __ws.WSToken(uuid.uuid4().hex)
         self.setClient = {}
+    
+    def __setattr__(self, name: str, value: Any) -> None:
+        pass
 
     async def WSRun(self, host : str, port : int ) -> None:
         pass
 
-    async def WSGetToken(self) -> str:
-        return self.ServerToken
+    async def WSGetToken(self) -> Any:
+        return self.ServerToken.get()
     
     async def GetTokenClient(self) -> dict:
+        """Menggambil token server yang sudah dibuat"""
         return self.setClient
     
     async def GenTokenClient(self, numClient : int) -> dict:
+        """Setiap kali dipanggil websocket server akan membuat token baru
+        bersama dengan jumlah klien yang ingin dibuat
+        """
         for i in range(numClient):
             hexUUID = uuid.uuid4().hex
             self.setClient[hexUUID[:5]] = hexUUID
 
         return self.setClient
+    
 
 
+
+#===================================================================
+# This part to create Asyncronouse TCP Server
+#===================================================================
 class AsyncServer(asyncio.Protocol):
     def connection_made(self, transport: asyncio.BaseTransport) -> None:
         peername = transport.get_extra_info("peername")
